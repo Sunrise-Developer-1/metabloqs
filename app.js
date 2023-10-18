f954c137ddfe992750d21a614b2b875e5060d8dd
Sunrise-Developer-1 <sunrisetechs.developer1@gmail.com>
2023-10-18 12:02:56 +0530

    node app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+let Port= 4000
+
+app.get("/",(req,res)=>{
+    res.status(200).send({
+        statu:true,
+        message: "App connected"
+    })
+})
+
+
+app.listen(Port, () => {
+    console.log(`Server is running on port ${Port}`);
+})
